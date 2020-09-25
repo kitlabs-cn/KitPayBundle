b7e48abbaac4ceb872417ef3a8bb88398e3c6d81
lcp0578 <lcp0578@gmail.com>
2020-09-25 18:31:45 +0800

    [MOD] add refund

--- Service/PaymentService.php
+++ Service/PaymentService.php
@@ -55,4 +55,55 @@ class PaymentService extends BaseService
            ];
        }
     }
+    
+    /**
+     * pay
+     *
+     * @param string $channel
+     * @param string $paytype
+     * @param array $metadata
+     * @param array $config
+     * @return number[]|string[]|number[]|string[]|number[]|string[]|mixed[]|array[]|number[]|string[]|NULL[][]
+     */
+    public function refund($channel, $paytype, $metadata, $config = [])
+    {
+        // check paytype
+        if(!$this->checkPayType($channel, $paytype)){
+            return [
+                'code' => 2,
+                'msg' => 'pay type error',
+                'data' => ''
+            ];
+        }
+        if(empty($config)){
+            $result = $this->getConfig($channel);
+            if(1 == $result['code']){
+                $config = $result['data'];
+            }else{
+                return $result;
+            }
+        }
+        // check config
+        $checkResult = $this->checkConfig($channel, $paytype);
+        if( 1 != $checkResult['code'] ) {
+            return $checkResult;
+        }
+        try {
+            $str = Refund::run(Config::WX_REFUND, $config, $metadata);
+            return [
+                'code' => 1,
+                'msg' => 'success',
+                'data' => $str
+            ];
+        } catch (PayException $e) {
+            return [
+                'code' => 0,
+                'msg' => 'exception',
+                'data' => [
+                    'code' => $e->getCode(),
+                    'message' => $e->getMessage()
+                ]
+            ];
+        }
+    }
 }
