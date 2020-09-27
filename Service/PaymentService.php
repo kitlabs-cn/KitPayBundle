c0e5de8cb90eb9ba43e04e2e95fd9e7745bdda44
lcp0578 <lcp0578@gmail.com>
2020-09-27 08:40:10 +0800

    [MOD] format

--- Service/PaymentService.php
+++ Service/PaymentService.php
@@ -8,86 +8,87 @@ use Payment\Config;
 
 class PaymentService extends BaseService
 {
+
     /**
-     * 
-     * @param string $channel
-     * @param string $paytype
-     * @param array $metadata
-     * @param array $config
+     *
+     * @param string $channel            
+     * @param string $paytype            
+     * @param array $metadata            
+     * @param array $config            
      * @return number[]|string[]|number[]|string[]|number[]|string[]|mixed[]|array[]|number[]|string[]|NULL[][]
      */
     public function run($channel, $paytype, $metadata, $config = [])
     {
         // check paytype
-       if(!$this->checkPayType($channel, $paytype)){
-           return [
-               'code' => 2,
-               'msg' => 'pay type error',
-               'data' => ''
-           ];
-       }
-       if(empty($config)){
-           $result = $this->getConfig($channel);
-           if(1 == $result['code']){
-               $config = $result['data'];
-           }else{
-               return $result;
-           }
-       }
-       // check config
-       $checkResult = $this->checkConfig($channel, $paytype);
-       if( 1 != $checkResult['code'] ) {
-           return $checkResult;
-       }
-       try {
-           $str = Charge::run($paytype, $config, $metadata);
-           return [
-               'code' => 1,
-               'msg' => 'success',
-               'data' => $str
-           ];
-       } catch (PayException $e) {
-           return [
-               'code' => 0,
-               'msg' => 'exception',
-               'data' => [
-                   'code' => $e->getCode(),
-                   'message' => $e->getMessage()
-               ]
-           ];
-       }
-    }
-    
-    /**
-     * pay
-     *
-     * @param string $channel
-     * @param string $paytype
-     * @param array $metadata
-     * @param array $config
-     * @return number[]|string[]|number[]|string[]|number[]|string[]|mixed[]|array[]|number[]|string[]|NULL[][]
-     */
-    public function refund($channel, $paytype, $metadata, $config = [])
-    {
-        // check paytype
-        if(!$this->checkPayType($channel, $paytype)){
+        if (! $this->checkPayType($channel, $paytype)) {
             return [
                 'code' => 2,
                 'msg' => 'pay type error',
                 'data' => ''
             ];
         }
-        if(empty($config)){
+        if (empty($config)) {
             $result = $this->getConfig($channel);
-            if(1 == $result['code']){
+            if (1 == $result['code']) {
                 $config = $result['data'];
-            }else{
+            } else {
                 return $result;
             }
         }
         // check config
         $checkResult = $this->checkConfig($channel, $paytype);
-        if( 1 != $checkResult['code'] ) {
+        if (1 != $checkResult['code']) {
+            return $checkResult;
+        }
+        try {
+            $str = Charge::run($paytype, $config, $metadata);
+            return [
+                'code' => 1,
+                'msg' => 'success',
+                'data' => $str
+            ];
+        } catch (PayException $e) {
+            return [
+                'code' => 0,
+                'msg' => 'exception',
+                'data' => [
+                    'code' => $e->getCode(),
+                    'message' => $e->getMessage()
+                ]
+            ];
+        }
+    }
+
+    /**
+     * pay
+     *
+     * @param string $channel            
+     * @param string $paytype            
+     * @param array $metadata            
+     * @param array $config            
+     * @return number[]|string[]|number[]|string[]|number[]|string[]|mixed[]|array[]|number[]|string[]|NULL[][]
+     */
+    public function refund($channel, $paytype, $metadata, $config = [])
+    {
+        // check paytype
+        if (! $this->checkPayType($channel, $paytype)) {
+            return [
+                'code' => 2,
+                'msg' => 'pay type error',
+                'data' => ''
+            ];
+        }
+        if (empty($config)) {
+            $result = $this->getConfig($channel);
+            if (1 == $result['code']) {
+                $config = $result['data'];
+            } else {
+                return $result;
+            }
+        }
+        // check config
+        $checkResult = $this->checkConfig($channel, $paytype);
+        if (1 != $checkResult['code']) {
             return $checkResult;
         }
         try {
